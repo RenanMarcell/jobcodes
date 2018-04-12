58f9c322e4788cbbb5df390d240ae1fe620a5941
renan <renanm@outlook.com>
2018-04-12 03:25:59 -0300

    fix leave button

--- mvp/js/authentication.js
+++ mvp/js/authentication.js
@@ -3,7 +3,6 @@ const auth = firebase.auth();
 $('#logout').click(function(){
     auth().signOut();
     sessionStorage.removeItem('email');
-    window.location.replace('https://www.jobcodes.com.br/mvp');
 });
 
 if (sessionStorage.getItem('email') === null) {
